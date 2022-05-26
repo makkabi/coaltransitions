28f8742815a9c2e6ecf4a31572281cb391334b3d
makkabi <j.weckerle@jpberlin.de>
2022-05-26 19:37:34 +0200

    Use Figure, fix typos

--- src/templates/publication/index.jsx
+++ src/templates/publication/index.jsx
@@ -11,6 +11,9 @@ import Richtext from '../../components/richtext';
 import style, { buttonIcon } from './style';
 import TagList from '../../components/tag-list';
 import withLayout from '../../components/with-layout';
+import { getImage } from 'gatsby-plugin-image';
+import Figure from '../../components/figure';
+import { captionStyle } from '../../components/findings-list/finding/style';
 
 const HUMAN_READABLE_LANGUAGES = {
   en: 'English',
@@ -35,122 +38,135 @@ const Page = ({
       tags: { nodes: tags },
     },
   },
-}) => (
-  <Constraint superwide>
-    <Helmet title={title} />
+}) => {
+  const image = getImage(featuredImage?.node?.localFile);
+  return (
+    <Constraint superwide>
+      <Helmet title={title} />
 
-    <article className="publication">
-      <style jsx>{style}</style>
+      <article className="publication">
+        <style jsx>{style}</style>
 
-      <header className="header">
-        <h1 className="title">
-          <span dangerouslySetInnerHTML={{ __html: title }} />
-        </h1>
+        <header className="header">
+          <h1 className="title">
+            <span dangerouslySetInnerHTML={{ __html: title }} />
+          </h1>
 
-        <div className="title-meta-container">
-          {(subtitle || publishedIn) && (
-            <p className="subtitle">
-              {subtitle} {subtitle && publishedIn && ' | '} {publishedIn}
-            </p>
-          )}
-          {year && (
-            <div className="year">
-              <small className="year-text">{year}</small>
+          <div className="title-meta-container">
+            {(subtitle || publishedIn) && (
+              <p className="subtitle">
+                {subtitle} {subtitle && publishedIn && ' | '} {publishedIn}
+              </p>
+            )}
+            {year && (
+              <div className="year">
+                <small className="year-text">{year}</small>
+              </div>
+            )}
+
+            {language && (
+              <div className="languages-container">
+                {buttonIcon.styles}
+
+                {language.map(({ language: downloadLanguage, externalUrl }) => (
+                  <Button
+                    key={`language-${downloadLanguage}`}
+                    to={externalUrl}
+                    theme="blue"
+                    external
+                  >
+                    {HUMAN_READABLE_LANGUAGES[downloadLanguage]}
+                    <DownloadIcon className={buttonIcon.className} />
+                  </Button>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {featuredImage?.node?.localFile && (
+            <div className="cover-image-container">
+              {image && (
+                <Figure
+                  altText={featuredImage.altText}
+                  image={image}
+                  caption={featuredImage.caption}
+                  captionClassName={captionStyle.className}
+                />
+              )}
             </div>
           )}
+        </header>
 
-          {language && (
-            <div className="languages-container">
-              {buttonIcon.styles}
-
-              {language.map(({ language: downloadLanguage, externalUrl }) => (
-                <Button
-                  key={`language-${downloadLanguage}`}
-                  to={externalUrl}
-                  theme="blue"
-                  external
-                >
-                  {HUMAN_READABLE_LANGUAGES[downloadLanguage]}
-                  <DownloadIcon className={buttonIcon.className} />
-                </Button>
-              ))}
+        <div className="body">
+          {abstract && (
+            <div className="abstract">
+              <Richtext content={abstract} />
             </div>
           )}
         </div>
 
-        {featuredImage?.node?.localFile && (
-          <div className="cover-image-container">
-            <Picture image={featuredImage.node.localFile} />
-          </div>
-        )}
-      </header>
+        <div className="meta">
+          {tags && (
+            <div className="meta-block">
+              <h3 className="meta-block-title">Keywords</h3>
 
-      <div className="body">
-        {abstract && (
-          <div className="abstract">
-            <Richtext content={abstract} />
-          </div>
-        )}
-      </div>
-
-      <div className="meta">
-        {tags && (
-          <div className="meta-block">
-            <h3 className="meta-block-title">Keywords</h3>
-
-            <div>
-              <TagList tags={tags} />
+              <div>
+                <TagList tags={tags} />
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {author && (
-          <div className="meta-block">
-            <h3 className="meta-block-title">Authors</h3>
-            <div>
-              <AuthorList authors={author} />
+          {author && (
+            <div className="meta-block">
+              <h3 className="meta-block-title">Authors</h3>
+              <div>
+                <AuthorList authors={author} />
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {institute && institute.length > 0 && (
-          <div className="meta-block">
-            <h3 className="meta-block-title">Insitute</h3>
-            <ul className="meta-block-list">
-              {institute.map(({ name }) => (
-                <li>
-                  <p className="meta-block-content">{name}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+          {institute && institute.length > 0 && (
+            <div className="meta-block">
+              <h3 className="meta-block-title">Institute</h3>
+              <ul className="meta-block-list">
+                {institute.map(({ name }) => (
+                  <li key={name}>
+                    <p className="meta-block-content">{name}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
 
-        {employer && (
-          <div className="meta-block">
-            <h3 className="meta-block-title">Employer</h3>
+          {employer && (
+            <div className="meta-block">
+              <h3 className="meta-block-title">Employer</h3>
 
-            <p className="meta-block-content">{employer}</p>
-          </div>
-        )}
-      </div>
-    </article>
-  </Constraint>
-);
+              <p className="meta-block-content">{employer}</p>
+            </div>
+          )}
+        </div>
+      </article>
+    </Constraint>
+  );
+};
 
 export default withLayout(Page);
 
 export const query = graphql`
-  query($databaseId: Int) {
+  query ($databaseId: Int) {
     publication: wpPublication(databaseId: { eq: $databaseId }) {
       title
       featuredImage {
         node {
+          altText
           localFile {
             childImageSharp {
-              fluid(maxWidth: 800) {
-                ...Picture
-              }
+              gatsbyImageData(
+                width: 800
+                placeholder: BLURRED
+                layout: CONSTRAINED
+              )
             }
           }
         }
