f6529edaaf9083a92cf948efdd570c32cac210de
makkabi <j.weckerle@jpberlin.de>
2022-05-27 15:55:57 +0200

    Use Figure

--- src/components/LogoGrid/index.jsx
+++ src/components/LogoGrid/index.jsx
@@ -1,37 +1,47 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 
-import Picture from '../picture';
 
 import style from './style';
+import Figure from '../figure';
+import { getImage } from 'gatsby-plugin-image';
 
-export const fragment = graphql`fragment LogoGrid on WpPage_Acf_Content_Logogrid {
-  logos {
-    link
-    logo {
-      localFile {
-        childImageSharp {
-          gatsbyImageData(width: 200, placeholder: BLURRED, layout: CONSTRAINED)
+export const fragment = graphql`
+  fragment LogoGrid on WpPage_Acf_Content_Logogrid {
+    logos {
+      link
+      logo {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              width: 200
+              placeholder: BLURRED
+              layout: CONSTRAINED
+            )
+          }
         }
       }
     }
   }
-}
 `;
 
 const LogoGrid = ({ logos, ...props }) => (
   <ul {...props}>
     <style jsx>{style}</style>
 
-    {logos.map(({ link, logo }) => (
-      <li>
-        <a href={link}>
-          <div>
-            <Picture image={logo?.localFile} />
-          </div>
-        </a>
-      </li>
-    ))}
+    {logos.map(({ link, logo }) => {
+      const image = getImage(logo?.localFile);
+      return (
+        <li key={link}>
+          <a href={link}>
+            <div>
+              <Figure altText={logo.altText} image={image} />
+            </div>
+          </a>
+        </li>
+      );
+    })}
   </ul>
 );
 
