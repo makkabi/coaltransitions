5d41d0476bd3629907ab69fbc0971edeaab2c7c7
makkabi <j.weckerle@jpberlin.de>
2022-05-31 17:43:05 +0200

    Display content

--- src/templates/strategy/index.jsx
+++ src/templates/strategy/index.jsx
@@ -1,6 +1,6 @@
 import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import AuthorList from '../../components/author-list';
 import Button from '../../components/button';
@@ -14,18 +14,18 @@ import withLayout from '../../components/with-layout';
 import { getImage } from 'gatsby-plugin-image';
 import Figure from '../../components/figure';
 import { captionStyle } from '../../components/findings-list/finding/style';
-
-const HUMAN_READABLE_LANGUAGES = {
-  en: 'English',
-  de: 'German',
-};
+import Picture from '../../components/picture';
 
 const Page = ({
   data: {
-    strategy: { title },
+    strategy: {
+      title,
+      featuredImage,
+      acf: { content, additionalContent, additionalLinks = [] },
+    },
   },
 }) => {
-  // const image = getImage(featuredImage?.node?.localFile);
+  const image = getImage(featuredImage?.node?.localFile);
   return (
     <Constraint superwide>
       <Helmet title={title} />
@@ -40,7 +40,7 @@ const Page = ({
 
           <div className="title-meta-container"></div>
 
-          {/*          {featuredImage?.node?.localFile && (
+          {featuredImage?.node?.localFile && (
             <div className="cover-image-container">
               {image && (
                 <Figure
@@ -51,10 +51,62 @@ const Page = ({
                 />
               )}
             </div>
-          )}*/}
+          )}
         </header>
 
-        <div className="body"></div>
+        <div className="body">
+          {content && (
+            <div className="description">
+              {content.map(({ __typename, ...block }) => {
+                switch (__typename) {
+                  case 'WpStrategy_Acf_Content_Text':
+                    return <Richtext content={block.text} />;
+
+                  case 'WpStrategy_Acf_Content_Image':
+                    const image = getImage(block?.image?.localFile);
+
+                    return image ? (
+                      <Figure
+                        altText={block.image.altText}
+                        image={image}
+                        caption={block.image.caption}
+                        captionClassName={captionStyle.className}
+                      />
+                    ) : null;
+
+                  default:
+                    return <p>Block not implemented</p>;
+                }
+              })}
+            </div>
+          )}
+
+          {additionalContent && (
+            <div className="description">
+              {additionalContent.map(({ __typename, ...block }) => {
+                switch (__typename) {
+                  case 'WpStrategy_Acf_AdditionalContent_Text':
+                    return <Richtext content={block.text} />;
+
+                  case 'WpStrategy_Acf_AdditionalContent_Image':
+                    const image = getImage(block?.image?.localFile);
+
+                    return image ? (
+                      <Figure
+                        altText={block.image.altText}
+                        image={image}
+                        caption={block.image.caption}
+                        captionClassName={captionStyle.className}
+                      />
+                    ) : null;
+
+                  default:
+                    return <p>Block not implemented</p>;
+                }
+              })}
+            </div>
+          )}
+        </div>
 
         <div className="meta"></div>
       </article>
@@ -68,6 +120,64 @@ export const query = graphql`
   query ($databaseId: Int) {
     strategy: wpStrategy(databaseId: { eq: $databaseId }) {
       title
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(width: 300, placeholder: BLURRED)
+            }
+          }
+        }
+      }
+      acf {
+        additionalLinks {
+          link
+          linktext
+        }
+        content {
+          __typename
+          ... on WpStrategy_Acf_Content_Text {
+            text
+          }
+          ... on WpStrategy_Acf_Content_Image {
+            image {
+              altText
+              caption
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    width: 800
+                    placeholder: BLURRED
+                    layout: CONSTRAINED
+                  )
+                }
+              }
+            }
+          }
+        }
+        additionalContent {
+          __typename
+          ... on WpStrategy_Acf_AdditionalContent_Text {
+            text
+          }
+          ... on WpStrategy_Acf_AdditionalContent_Image {
+            image {
+              altText
+              caption
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    width: 800
+                    placeholder: BLURRED
+                    layout: CONSTRAINED
+                  )
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 `;
