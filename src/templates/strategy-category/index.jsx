c8c228306d31f3a1e44060018ac5f157756734e1
makkabi <j.weckerle@jpberlin.de>
2022-06-07 18:56:19 +0200

    Add StrategyCategory template

---
+++ src/templates/strategy-category/index.jsx
@@ -0,0 +1,122 @@
+import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
+import React, { Fragment } from 'react';
+
+import Constraint from '../../components/constraint';
+import Intro from '../../components/intro';
+import MoreLinksList from '../../components/more-links-list';
+import Picture from '../../components/picture';
+import PublicationList from '../../components/publication-list';
+import Richtext from '../../components/richtext';
+import withLayout from '../../components/with-layout';
+
+import style, { featuredImage as featuredImageStyle } from './style';
+import { getImage } from 'gatsby-plugin-image';
+import Figure from '../../components/figure';
+import { captionStyle } from '../../components/findings-list/finding/style';
+
+const Page = ({
+  data: {
+    finding: {
+      title,
+      featuredImage,
+      acf: { intro, content },
+    },
+  },
+}) => {
+  const image = getImage(featuredImage?.node?.localFile);
+
+  return (
+    <Fragment>
+      <style jsx>{style}</style>
+      {featuredImageStyle.styles}
+      <Helmet title={title} />
+
+      <header className="header">
+        <h1 className="title" dangerouslySetInnerHTML={{ __html: title }} />
+
+        {image && (
+          <Figure
+            altText={featuredImage.altText}
+            image={image}
+            caption={featuredImage.caption}
+            captionClassName={captionStyle.className}
+          />
+        )}
+      </header>
+
+      <Constraint topLevel>
+        <div className="body">
+          <Intro intro={intro} />
+
+          {content && (
+            <div className="description">
+              {content.map(({ __typename, ...block }) => {
+                switch (__typename) {
+                  case 'WpStrategyCategory_Acf_Content_Text':
+                    return <Richtext content={block.text} />;
+
+                  case 'WpStrategyCategory_Acf_Content_Image':
+                    return (
+                      <Picture
+                        image={block.image.localFile}
+                        caption={block.image.caption}
+                      />
+                    );
+
+                  default:
+                    return <p>Block not implemented</p>;
+                }
+              })}
+            </div>
+          )}
+        </div>
+      </Constraint>
+    </Fragment>
+  );
+};
+
+export default withLayout(Page);
+
+export const query = graphql`
+  query ($databaseId: Int) {
+    finding: wpStrategyCategory(databaseId: { eq: $databaseId }) {
+      title
+      featuredImage {
+        node {
+          caption
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+            }
+          }
+        }
+      }
+      acf {
+        intro
+        content {
+          __typename
+          ... on WpStrategyCategory_Acf_Content_Text {
+            text
+          }
+          ... on WpStrategyCategory_Acf_Content_Image {
+            image {
+              altText
+              caption
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    width: 800
+                    placeholder: BLURRED
+                    layout: CONSTRAINED
+                  )
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/templates/strategy-category/style.js
@@ -0,0 +1,129 @@
+import css from 'styled-jsx/css';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import { colors, mixins, mq } from '../../token';
+
+export default css`
+  .header {
+    margin-bottom: 1.5rem;
+    width: 100%;
+  }
+
+  @media ${mq.tablet} {
+    margin-bottom: 4.5rem;
+  }
+
+  .title {
+    ${mixins.text('huge')}
+
+    align-self: flex-end;
+    background-color: ${colors.blueBrand};
+    color: white;
+    margin-bottom: -2.5rem;
+    margin-left: auto;
+    margin-top: 0;
+    padding: 1rem;
+    position: relative;
+    width: 90%;
+    z-index: 10;
+  }
+
+  @media ${mq.tablet} {
+    .title {
+      ${mixins.text('huge', 'tablet')}
+
+      padding: 1.5rem 5rem 1.5rem 1.5rem;
+      width: 75%;
+    }
+  }
+
+  @media ${mq.desktop} {
+    .title {
+      ${mixins.text('huge', 'desktop')}
+
+      padding: 1.5rem 15rem 1.5rem 2.5rem;
+    }
+  }
+
+  .body {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  @media ${mq.tablet} {
+    .body {
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
+    }
+  }
+
+  @media ${mq.desktop} {
+    .body {
+      padding-left: 0;
+      padding-right: 0;
+    }
+  }
+
+  .description {
+    margin-bottom: 3rem;
+  }
+
+  @media ${mq.tablet} {
+    .description {
+      margin-bottom: 5rem;
+    }
+  }
+
+  @media ${mq.desktop} {
+    .description {
+      margin-bottom: 6rem;
+    }
+  }
+
+  .additional-links-container {
+    margin-bottom: 2rem;
+    width: 100%;
+  }
+
+  .section-headline {
+    ${mixins.text('regular-big')};
+
+    align-items: center;
+    color: ${colors.blueBrand};
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0;
+    text-transform: uppercase;
+  }
+
+  @media ${mq.tablet} {
+    .section-headline {
+      ${mixins.text('regular-big', 'tablet')};
+    }
+  }
+
+  @media ${mq.desktop} {
+    .section-headline {
+      ${mixins.text('regular-big', 'desktop')};
+    }
+  }
+
+  .publications-list-container {
+    margin-left: -1rem;
+  }
+
+  @media ${mq.desktop} {
+    .publications-list-container {
+      margin-left: calc(-15% - 10.5rem);
+    }
+  }
+`;
+
+export const featuredImage = css.resolve`
+  height: auto;
+  width: 100%;
+`;
